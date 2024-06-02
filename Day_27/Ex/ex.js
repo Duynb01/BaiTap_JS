96ad4d89479a6c8c49e25d4d563abc2ff3ea1e27
Duynb01 <bduy0001@gmail.com>
2024-06-02 11:38:24 +0700

    Day 27

--- Day_27/Ex/ex.js
+++ Day_27/Ex/ex.js
@@ -2,6 +2,7 @@ var modal = document.querySelector(".modal");
 var closeBtn = document.querySelector(".close");
 var overlay = document.querySelector(".modal_overlay");
 var btn = document.querySelector(".btn");
+
 var btnLogin = document.querySelector(".btn_login");
 var btnRegister = document.querySelector(".btn_register");
 var login = document.querySelector(".login");
@@ -10,6 +11,8 @@ var i = modal.querySelectorAll("i");
 var form = document.querySelectorAll("form");
 var password = modal.querySelectorAll("input.password");
 var email = modal.querySelectorAll("input.email");
+var er1 = modal.querySelectorAll(".error1");
+var er2 = modal.querySelector(".error2");
 var originalLogin = login.innerHTML;
 var originalRegister = register.innerHTML;
 btn.addEventListener("click", function () {
@@ -33,12 +36,14 @@ btnRegister.addEventListener("click", function () {
   btnLogin.classList.remove("click");
   login.classList.add("hidden");
   register.classList.add("show");
+  register.innerHTML = originalRegister;
 });
 btnLogin.addEventListener("click", function () {
   btnLogin.classList.add("click");
   btnRegister.classList.remove("click");
   login.classList.remove("hidden");
   register.classList.remove("show");
+  login.innerHTML = originalLogin;
 });
 i.forEach(function (item) {
   item.addEventListener("click", function () {
@@ -62,12 +67,71 @@ form.forEach(function (item) {
   });
 });
 email.forEach(function (item) {
+  item.addEventListener("focus", function () {
+    item.addEventListener("blur", function () {
+      if (item.value.length === 0) {
+        er1.forEach(function (er) {
+          er.classList.add("show");
+        });
+      } else {
+        er1.forEach(function (er) {
+          er.classList.remove("show");
+        });
+      }
+    });
+  });
   item.addEventListener("input", function () {
-    console.log(item.value);
+    if (item.value.length > 0) {
+      er1.forEach(function (er) {
+        er.classList.remove("show");
+      });
+    }
+  });
+});
+email.forEach(function (item) {
+  item.addEventListener("input", function () {
+    var text = item.value;
+    if (text.length > 0) {
+      if (!text.includes(key1) || !text.includes(key2)) {
+        er2.classList.add("show");
+      } else {
+        er2.classList.remove("show");
+      }
+    } else {
+      er2.classList.remove("show");
+    }
   });
 });
 password.forEach(function (item) {
-  var mes = item.addEventListener("input", function () {
+  item.addEventListener("focus", function () {
+    item.addEventListener("blur", function () {
+      if (item.value.length === 0) {
+        er1.forEach(function (er) {
+          er.classList.add("show");
+        });
+      } else {
+        er1.forEach(function (er) {
+          er.classList.remove("show");
+        });
+      }
+    });
+  });
+  item.addEventListener("input", function () {
+    if (item.value.length > 0) {
+      er1.forEach(function (er) {
+        er.classList.remove("show");
+      });
+    }
+  });
+  //   });
+  //   item.addEventListener("input", function () {
+  //     if (item.value.length > 0) {
+  //       er1.classList.remove("show");
+  //     }
+  //   });
+});
+password.forEach(function (item) {
+  item.addEventListener("input", function () {
     item.addEventListener("blur", function () {
       if (item.value.length === 0) {
         console.log(`Nhập mật khẩu`);
@@ -77,3 +141,5 @@ password.forEach(function (item) {
     });
   });
 });
+var key1 = "@";
+var key2 = ".co";
