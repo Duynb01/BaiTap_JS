18420d4c317d30c9b6616d1e21f95294a38615ef
Duynb01 <bduy0001@gmail.com>
2024-07-13 23:47:55 +0700

    Day 39

--- Day_38/Ex/feature.js
+++ Day_38/Ex/feature.js
@@ -1,6 +1,5 @@
 const urlApi = "http://localhost:3000";
-const unfinish = document.querySelector(".unfinished");
-const finish = document.querySelector(".finish");
+
 const getData = async () => {
   const response = await fetch(urlApi + "/todo-list");
   const data = await response.json();
@@ -9,10 +8,15 @@ const getData = async () => {
 getData();
 
 const renderList = async (object) => {
-  object.map(({ id, name, status }) => {
+  const unfinish = document.querySelector(".unfinished");
+  const finish = document.querySelector(".finish");
+  object.map(({ status }) => {
     if (status === "finish") {
-      finish.innerHTML += `
-            <div class="name-work">
+      finish.innerHTML = `
+            ${object
+              .map(
+                ({ id, name }) =>
+                  `<div class="name-work">
                 <span class="content">${name
                   .replaceAll("<", "&lt;")
                   .replaceAll(">", "&gt;")}</span>
@@ -27,54 +31,83 @@ const renderList = async (object) => {
                     <i class="fa-solid fa-check-to-slot"></i>
                     </button>
                 </div>
-            </div>
-            `;
+            </div>`
+              )
+              .join("")}`;
     } else {
-      unfinish.innerHTML += `
-         <div class="name-work">
-                <span class="content">${name
-                  .replaceAll("<", "&lt;")
-                  .replaceAll(">", "&gt;")}</span>
-                <div class="item-option">
-                    <button class="btn btn-delete delete">
-                    <i class="fa-regular fa-trash-can"></i>
-                    </button>
-                    <button class="btn btn-edit icon">
-                    <i class="fa-regular fa-pen-to-square"></i>
-                    </button>
-                    <button class="btn btn-check check">
-                    <i class="fa-solid fa-check-to-slot"></i>
-                    </button>
-                </div>
-            </div>`;
+      unfinish.innerHTML = `
+        ${object
+          .map(
+            ({ id, name }) =>
+              `<div class="name-work">
+            <span class="content">${name
+              .replaceAll("<", "&lt;")
+              .replaceAll(">", "&gt;")}</span>
+            <div class="item-option">
+                <button class="btn btn-delete delete">
+                <i class="fa-regular fa-trash-can"></i>
+                </button>
+                <button class="btn btn-edit icon">
+                <i class="fa-regular fa-pen-to-square"></i>
+                </button>
+                <button class="btn btn-check check">
+                <i class="fa-solid fa-check-to-slot"></i>
+                </button>
+            </div>
+        </div>`
+          )
+          .join("")}`;
     }
   });
-  //   unfinish.innerHTML = `${object
-  //     .map(
-  //       ({ id, name, status }) =>
-  //         `
-  //             <div class="name-work">
-  //                 <span class="content">${name
-  //                   .replaceAll("<", "&lt;")
-  //                   .replaceAll(">", "&gt;")}</span>
-  //                 <div class="item-option">
-  //                     <button class="btn btn-delete delete">
-  //                     <i class="fa-regular fa-trash-can"></i>
-  //                     </button>
-  //                     <button class="btn btn-edit icon">
-  //                     <i class="fa-regular fa-pen-to-square"></i>
-  //                     </button>
-  //                     <button class="btn btn-check check ${
-  //                       status === "finish" ? "access" : ""
-  //                     }">
-  //                     <i class="fa-solid fa-check-to-slot"></i>
-  //                     </button>
-  //                 </div>
-  //             </div>
-  //             `
-  //     )
-  //     .join("")}`;
 };
+// else {
+//   unfinish.innerHTML += `
+//      <div class="name-work">
+//             <span class="content">${name
+//               .replaceAll("<", "&lt;")
+//               .replaceAll(">", "&gt;")}</span>
+//             <div class="item-option">
+//                 <button class="btn btn-delete delete">
+//                 <i class="fa-regular fa-trash-can"></i>
+//                 </button>
+//                 <button class="btn btn-edit icon">
+//                 <i class="fa-regular fa-pen-to-square"></i>
+//                 </button>
+//                 <button class="btn btn-check check">
+//                 <i class="fa-solid fa-check-to-slot"></i>
+//                 </button>
+//             </div>
+//         </div>`;
+// }
+
+// ;
+
+//   unfinish.innerHTML = `${object
+//     .map(
+//       ({ id, name, status }) =>
+//         `
+//             <div class="name-work">
+//                 <span class="content">${name
+//                   .replaceAll("<", "&lt;")
+//                   .replaceAll(">", "&gt;")}</span>
+//                 <div class="item-option">
+//                     <button class="btn btn-delete delete">
+//                     <i class="fa-regular fa-trash-can"></i>
+//                     </button>
+//                     <button class="btn btn-edit icon">
+//                     <i class="fa-regular fa-pen-to-square"></i>
+//                     </button>
+//                     <button class="btn btn-check check ${
+//                       status === "finish" ? "access" : ""
+//                     }">
+//                     <i class="fa-solid fa-check-to-slot"></i>
+//                     </button>
+//                 </div>
+//             </div>
+//             `
+//     )
+//     .join("")}`;
+
 console.log(finish.children);
 console.log(finish.children.length);
 var quantity = finish.children;
---
+++ Day_39/Ex/ex.js
@@ -0,0 +1,72 @@
+var listBlog = document.querySelector(".list-blog");
+
+const urlApi = "http://localhost:3000";
+
+const getLimit = () => {
+  var limit = 5;
+  const handler = () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollPercent = (scrollTop / (scrollHeight - clientHeight)) * 100;
+    if (scrollPercent > 90) {
+      limit += 5;
+      window.removeEventListener("scroll", handler);
+      setTimeout(() => {
+        main();
+      }, 300);
+    }
+  };
+  window.addEventListener("scroll", handler);
+  return limit;
+};
+
+const getApi = async (newLimit) => {
+  const response = await fetch(`${urlApi}/blogs?_page=1&_limit=${newLimit}`);
+  const blog = await response.json();
+  blog.forEach((element) => {
+    renderBlog(element);
+  });
+};
+
+const getHashTag = (array) => {
+  return array
+    .map((hashTag) => {
+      return `<button class="hash-tag"># ${hashTag}</button>`;
+    })
+    .join("");
+};
+
+const renderBlog = async (object) => {
+  listBlog.innerHTML += `
+        <section>
+            <article class="inf">
+                <div class="head">
+                    <span class="avatar">${object.name
+                      .trim()
+                      .slice(0, 1)
+                      .toLowerCase()}</span>
+                    <span class="name">${object.name.trim()}</span>
+                </div>
+                <div class="main">
+                    <h3 class="title">${object.title}</h3>
+                    <div  class="content">${object.content}</div>
+                </div>
+                <div class="footer">
+                    <div class="tag">
+                        ${getHashTag(object.hashtag)}
+                    </div>
+                    <div class="time">
+                         <span>Khoảng ${object.timeRead} giây đọc.</span>
+                    </div>
+                </div>
+            <article>
+        </section>
+    `;
+};
+
+const main = async () => {
+  const newLimit = await getLimit();
+  getApi(newLimit);
+};
+main();
